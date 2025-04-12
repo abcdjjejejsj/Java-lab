1aa83286ae7ab9cbec6e063eb5a08cbe2cb2b12e
Vaibhav Salve <144205049+abcdjjejejsj@users.noreply.github.com>
2025-04-12 09:36:50 +0530

    Update Assignment_6.java

--- Assignment_6.java
+++ Assignment_6.java
@@ -1,61 +1,78 @@
-// Using Thread Class
-class MyThread1 extends Thread {
+import java.util.Scanner;
+
+// Factorial thread (extends Thread)
+class FactorialThread extends Thread {
+    private int number;
+
+    public FactorialThread(int number) {
+        super("FactorialThread"); // Set thread name
+        this.number = number;
+    }
+
+    @Override
     public void run() {
-        System.out.println(getName() + " is starting...");
-
         try {
-            Thread.sleep(1000);  // sleep for 1 sec
+            System.out.println(getName() + " started.");
+            long factorial = 1;
+            for (int i = 1; i <= number; i++) {
+                factorial *= i;
+                System.out.println(getName() + " calculating: " + i + "! = " + factorial);
+                Thread.sleep(500); // simulate time-consuming task
+            }
+            System.out.println(getName() + " result: Factorial of " + number + " is " + factorial);
         } catch (InterruptedException e) {
-            System.out.println(e);
+            System.out.println(getName() + " interrupted.");
         }
-
-        System.out.println(getName() + " is finished.");
     }
 }
 
-// Using Runnable Interface
-class MyThread2 implements Runnable {
+// Square thread (implements Runnable)
+class SquareThread implements Runnable {
+    private int number;
+
+    public SquareThread(int number) {
+        this.number = number;
+    }
+
+    @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " is starting...");
-
+        Thread.currentThread().setName("SquareThread"); // Set thread name
         try {
-            Thread.sleep(1500);  // sleep for 1.5 sec
+            System.out.println(Thread.currentThread().getName() + " started.");
+            for (int i = 1; i <= number; i++) {
+                int square = i * i;
+                System.out.println(Thread.currentThread().getName() + " calculating: " + i + "^2 = " + square);
+                Thread.sleep(400); // simulate task time
+            }
+            System.out.println(Thread.currentThread().getName() + " finished.");
         } catch (InterruptedException e) {
-            System.out.println(e);
+            System.out.println(Thread.currentThread().getName() + " interrupted.");
         }
-
-        System.out.println(Thread.currentThread().getName() + " is finished.");
     }
 }
 
-public class ThreadConcepts {
+public class Main {
     public static void main(String[] args) {
-        MyThread1 t1 = new MyThread1(); // Thread class
-        MyThread2 obj = new MyThread2();
-        Thread t2 = new Thread(obj);    // Runnable interface
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to calculate factorial and squares up to it: ");
+        int num = scanner.nextInt();
 
-        t1.setName("Thread-One");
-        t2.setName("Thread-Two");
+        // Create both threads
+        FactorialThread factorialThread = new FactorialThread(num);
+        Thread squareThread = new Thread(new SquareThread(num));
 
-        System.out.println("Starting Threads...");
-        
-        t1.start();
-        t2.start();
+        // Start both threads
+        factorialThread.start();
+        squareThread.start();
 
-        System.out.println("Is t1 alive? " + t1.isAlive());
-        System.out.println("Is t2 alive? " + t2.isAlive());
-
+        // Wait for both threads to finish
         try {
-            t1.join();  // wait for t1
-            t2.join();  // wait for t2
+            factorialThread.join();
+            squareThread.join();
         } catch (InterruptedException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
 
-        System.out.println("Are threads alive after finish?");
-        System.out.println("Is t1 alive? " + t1.isAlive());
-        System.out.println("Is t2 alive? " + t2.isAlive());
-
-        System.out.println("Main Thread Finished.");
+        System.out.println("Main thread done.");
     }
 }
